fe1f3a1051b323282532d1b2c6d6ada2f6ac0076
saeed1202 <sakhshijan@gmail.com>
2024-09-01 22:19:20 +0330

    fixed overflow issue

--- app/(root)/_components/projectContainer.tsx
+++ app/(root)/_components/projectContainer.tsx
@@ -13,20 +13,19 @@ export function ProjectContainer({
  });
  const x = useTransform(
   scrollYProgress,
-  [0, 0.15, 0.85, 1],
-  ["0%", "-5%", `-${(len - 1) * 100 - 5}%`, `-${(len - 1) * 100}%`],
+  [0, 0.05, 1],
+  ["0%", "0%", `${100 / len - 100}%`],
  );
  return (
   <div ref={target} style={{ minHeight: `${len * 100 + 220}vh` }}>
    <h2 className="my-16 text-center text-[clamp(2.5rem,10vw,8rem)] font-bold">
     Projects I'v Done
    </h2>
-   <motion.div
-    style={{ x }}
-    className="sticky top-0 flex min-h-lvh items-center"
-   >
-    {children}
-   </motion.div>
+   <div className="sticky top-0 flex min-h-lvh w-screen items-center overflow-hidden">
+    <motion.div style={{ x }} className="flex">
+     {children}
+    </motion.div>
+   </div>
   </div>
  );
 }
